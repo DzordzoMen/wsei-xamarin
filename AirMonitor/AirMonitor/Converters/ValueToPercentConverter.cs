6feb6bc3c679fc8700ace9de05604a1ebf13be98
Maciej <neipreis22@gmail.com>
2020-06-22 17:30:43 +0200

    Lab2-4 MVVM for DetailsPage ;_;

---
+++ AirMonitor/AirMonitor/Converters/ValueToPercentConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AirMonitor.Converters
+{
+    class ValueToPercentConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            double.TryParse(value.ToString(), out var parsedString);
+            return parsedString * 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AirMonitor.ViewModels
+{
+    class DetailsViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _caqiValue = 57;
+        public int CaqiValue {
+            set {
+                if (_caqiValue == value) return;
+
+                _caqiValue = value;
+                ExecutePropertyChanged("CaqiValue");
+            }
+            get {
+                return _caqiValue;
+            }
+        }
+
+        private string _caqiTitle = "Świetna jakość!";
+
+        public string CaqiTitle {
+            set {
+                if (_caqiTitle == value) return;
+
+                _caqiTitle = value;
+                ExecutePropertyChanged("CaqiTitle");
+            }
+            get {
+                return _caqiTitle;
+            }
+        }
+
+        private string _caqiDescription = "Możesz bezpiecznie wyjść z domu bez swojej maski anty-smogowej i nie bać się o swoje zdrowie.";
+
+        public string CaqiDescription {
+            set {
+                if (_caqiDescription == value) return;
+
+                _caqiDescription = value;
+                ExecutePropertyChanged("CaqiDescription");
+            }
+
+            get {
+                return _caqiDescription;
+            }
+        }
+
+        private int _pm25Value = 34;
+
+        public int Pm25Value {
+            set {
+                if (_pm25Value == value) return;
+
+                _pm25Value = value;
+                ExecutePropertyChanged("Pm25Value");
+            }
+
+            get {
+                return _pm25Value;
+            }
+        }
+
+        private int _pm25Percent = 137;
+
+        public int Pm25Percent {
+            set {
+                if (_pm25Percent == value) return;
+
+                _pm25Percent = value;
+                ExecutePropertyChanged("Pm25Percent");
+            }
+
+            get {
+                return _pm25Percent;
+            }
+        }
+
+        private int _pm10Value = 67;
+
+        public int Pm10Value {
+            set {
+                if (_pm10Value == value) return;
+
+                _pm10Value = value;
+                ExecutePropertyChanged("Pm10Value");
+            }
+
+            get {
+                return _pm10Value;
+            }
+        }
+
+        private int _pm10Percent = 135;
+
+        public int Pm10Percent {
+            set {
+                if (_pm10Percent == value) return;
+
+                _pm10Percent = value;
+                ExecutePropertyChanged("Pm10Percent");
+            }
+
+            get {
+                return _pm10Percent;
+            }
+        }
+
+        private Double _wetnessValue = 0.95;
+
+        public Double WetnessValue {
+            set {
+                if (_wetnessValue == value) return;
+
+                _wetnessValue = value;
+                ExecutePropertyChanged("WetnessValue");
+            }
+
+            get {
+                return _wetnessValue;
+            }
+        }
+
+        private int _pressureValue = 1026;
+
+        public int PressureValue {
+            set {
+                if (_pressureValue == value) return;
+
+                _pressureValue = value;
+                ExecutePropertyChanged("Pm10Value");
+            }
+
+            get {
+                return _pressureValue;
+            }
+        }
+
+        public DetailsViewModel() { }
+
+
+        private void ExecutePropertyChanged(string propName) {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
