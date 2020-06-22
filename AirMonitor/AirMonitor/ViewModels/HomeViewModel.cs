ee78a81d6a97019bc1cd70ef3d6daf88df26ae41
Maciej <neipreis22@gmail.com>
2020-06-22 16:07:27 +0200

    Lab2-4 MVVM for HomePage

---
+++ AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AirMonitor.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AirMonitor.ViewModels
+{
+    class HomeViewModel {
+        private INavigation _navigation;
+        public HomeViewModel(INavigation navigation) {
+            _navigation = navigation;
+        }
+
+        public ICommand GoToDetailsPage => new Command(async () => await _navigation.PushAsync(new DetailsPage()));
+    }
+}
--- AirMonitor/AirMonitor/Views/HomePage.xaml.cs
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using AirMonitor.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,10 +10,7 @@ namespace AirMonitor.Views
         public HomePage()
         {
             InitializeComponent();
-        }
-
-        private async void GoToDetailsPage(object sender, EventArgs e) {
-            await Navigation.PushAsync(new NavigationPage(new DetailsPage()));
+            BindingContext = new HomeViewModel(Navigation);
         }
     }
 }
